9d46e4ab2d2908c95b562a36c6b8fedae8161ddd
Ivan Vorobyev <ivorob@gmail.com>
2022-10-11 21:45:22 +0300

    Extract algorithm to function

--- ImageToner.py
+++ ImageToner.py
@@ -4,29 +4,29 @@ import argparse
 import image
 import os
 
+def grayscale(currentPixel):
+    grayscale = int(0.299 * currentPixel.red +
+                    0.587 * currentPixel.green +
+                    0.114 * currentPixel.blue)
+    return image.Pixel(grayscale, grayscale, grayscale)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('filename')
 # parser.add_argument('scheme', choices=['negative', 'greyscale'])
 args = parser.parse_args()
 
-image = image.FileImage(args.filename)
-width = image.get_width()
-height = image.get_height()
+newImage = image.FileImage(args.filename)
+width = newImage.get_width()
+height = newImage.get_height()
 
-newImage = image.copy()
+imageCopy = newImage.copy()
 
 print("Processing...\t0%", end='')
 lastPercent = 0
 for row in range(height):
     for column in range(width):
-        currentPixel = newImage.getPixel(column, row)
-        greyscale = int(0.299 * currentPixel.red +
-                        0.587 * currentPixel.green +
-                        0.114 * currentPixel.blue)
-        currentPixel.red = greyscale
-        currentPixel.green = greyscale
-        currentPixel.blue = greyscale
-        newImage.setPixel(column, row, currentPixel)
+        currentPixel = grayscale(imageCopy.getPixel(column, row))
+        imageCopy.setPixel(column, row, currentPixel)
 
     newPercent = row * 100 // height
     if newPercent > lastPercent:
@@ -38,5 +38,5 @@ print("\rProcessing...\t100 %")
 fileparts = os.path.splitext(args.filename)
 newFileName = fileparts[0] + "_new" + fileparts[1]
 print("Saving to '" + newFileName + "'...\t", end='')
-newImage.save(newFileName)
+imageCopy.save(newFileName)
 print("OK")
